06e962ec28b7c00376ae4b302923f8f20684e276
Unknown <cavallogianmarco@gmail.com>
2018-08-09 22:48:42 +0200

    Modifiche
    (non funziona completamente)

--- dist/index.js
+++ dist/index.js
@@ -14,60 +14,75 @@ var _util = require('util');
 
 var _util2 = _interopRequireDefault(_util);
 
+var _bluebird = require('bluebird');
+
+var _bluebird2 = _interopRequireDefault(_bluebird);
+
+var _opn = require('opn');
+
+var _opn2 = _interopRequireDefault(_opn);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_readline2.default.Interface.prototype.question[_util.promisify.custom] = function (prompt) {
+  var _this = this;
+
+  return new _bluebird.Promise(function (resolve) {
+    return _readline2.default.Interface.prototype.question.call(_this, prompt, resolve);
+  });
+};
+_readline2.default.Interface.prototype.questionAsync = (0, _util.promisify)(_readline2.default.Interface.prototype.question);
+
+// PROMISIFICATION
+var readFileAsync = _util2.default.promisify(_fs2.default.readFile);
 
 var SCOPES = ['https://www.googleapis.com/auth/drive.file'];
 var TOKEN_PATH = 'token.json';
 
-function getAccessToken(oAuth2Client, callback) {
+async function getAccessToken(oAuth2Client) {
   var authUrl = oAuth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES
   });
-  console.log('Authorize this app by visiting this url:', authUrl);
+
+  await (0, _opn2.default)(authUrl);
   var rl = _readline2.default.createInterface({
     input: process.stdin,
     output: process.stdout
   });
-  rl.question('Enter the code from that page here: ', function (code) {
-    rl.close();
-    oAuth2Client.getToken(code, function (err, token) {
-      if (err) return console.error('Error retrieving access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      _fs2.default.writeFile(TOKEN_PATH, JSON.stringify(token), function (err) {
-        if (err) console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
+  var code = await rl.questionAsync('Enter the code from that page here: ');
+  rl.close();
+  return oAuth2Client.getToken(code, async function (err, token) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    oAuth2Client.setCredentials(token);
+    // Store the token to disk for later program executions
+    await _util2.default.promisify(_fs2.default.writeFile)(TOKEN_PATH, JSON.stringify(token));
+    return oAuth2Client;
   });
 }
 
-function listFiles(auth) {
+async function listFiles(auth) {
   var fileMetadata = {
     'name': 'RENDER.png'
   };
   var media = {
     mimeType: 'image/jpeg',
-    body: _fs2.default.createReadStream('../../pictures/RENDER.png')
+    body: _fs2.default.createReadStream('../../../pictures/RENDER.png')
   };
   var drive = _googleapis.google.drive({ version: 'v3', auth: auth });
-  drive.files.create({
+  var createDFile = _bluebird.Promise.promisify(drive.files.create);
+  var file = await createDFile({
     resource: fileMetadata,
     media: media,
     fields: 'id'
-  }, function (err, file) {
-    if (err) {
-      // Handle error
-      console.error(err);
-    } else {
-      console.log('File: ', file.data.id);
-    }
   });
+  console.log(file.data.id);
 };
 
-function authorize(credentials, callback) {
+async function authorize(credentials) {
   var _credentials$installe = credentials.installed,
       client_secret = _credentials$installe.client_secret,
       client_id = _credentials$installe.client_id,
@@ -76,18 +91,20 @@ function authorize(credentials, callback) {
   var oAuth2Client = new _googleapis.google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
   // Check if we have previously stored a token.
-  _fs2.default.readFile(TOKEN_PATH, function (err, token) {
-    if (err) return getAccessToken(oAuth2Client, callback);
+  try {
+    var token = await readFileAsync(TOKEN_PATH);
     oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
+  } catch (err) {
+    return getAccessToken(oAuth2Client);
+  }
+  return oAuth2Client;
 };
 
 (async function () {
-
   try {
-    var credential = await _util2.default.promisify(_fs2.default.readFile)('credentials.json');
-    authorize(JSON.parse(credential), listFiles);
+    var credential = await readFileAsync('credentials.json');
+    var autorizzazione = await authorize(JSON.parse(credential));
+    await listFiles(autorizzazione);
   } catch (error) {
     console.log(error);
   }
--- src/index.js
+++ src/index.js
@@ -1,87 +1,89 @@
- import fs from 'fs';
- import readline from 'readline';
- import {google} from 'googleapis';
- import util from "util";
- import bluebird from "bluebird";
- 
- const SCOPES = ['https://www.googleapis.com/auth/drive.file'];
- const TOKEN_PATH = 'token.json';
+import fs from 'fs';
+import readline from 'readline';
+import { google } from 'googleapis';
+import util, { promisify } from "util";
+import bluebird, { Promise } from "bluebird";
+import opn from 'opn';
 
- function getAccessToken(oAuth2Client, callback) {
+readline.Interface.prototype.question[promisify.custom] = function (prompt) {
+  return new Promise(resolve => readline.Interface.prototype.question.call(this, prompt, resolve), );
+};
+readline.Interface.prototype.questionAsync = promisify(readline.Interface.prototype.question, );
+
+// PROMISIFICATION
+const readFileAsync = util.promisify(fs.readFile);
+
+const SCOPES = ['https://www.googleapis.com/auth/drive.file'];
+const TOKEN_PATH = 'token.json';
+
+async function getAccessToken(oAuth2Client) {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES,
   });
-  console.log('Authorize this app by visiting this url:', authUrl);
+
+  await opn(authUrl);
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
+  const code = await rl.questionAsync('Enter the code from that page here: ');
+  rl.close();
+  return oAuth2Client.getToken(code, async (err, token) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    oAuth2Client.setCredentials(token);
+    // Store the token to disk for later program executions
+    await util.promisify(fs.writeFile)(TOKEN_PATH, JSON.stringify(token));
+    return oAuth2Client;
   });
 }
 
 
 
-function listFiles(auth) {
+async function listFiles(auth) {
   var fileMetadata = {
     'name': 'RENDER.png'
   };
   var media = {
     mimeType: 'image/jpeg',
-    body: fs.createReadStream('../../pictures/RENDER.png')
+    body: fs.createReadStream('../../../pictures/RENDER.png')
   };
-  const drive = google.drive({version: 'v3', auth});
-  drive.files.create({
+  const drive = google.drive({ version: 'v3', auth });
+  const createDFile = Promise.promisify(drive.files.create);
+  const file = await createDFile({
     resource: fileMetadata,
     media: media,
     fields: 'id'
-  }, function (err, file) {
-    if (err) {
-      // Handle error
-      console.error(err);
-    } else {
-      console.log('File: ', file.data.id);
-    }
-      });
-  };
+  });
+  console.log(file.data.id);
+};
 
 
-  function authorize(credentials, callback) {
-    const {client_secret, client_id, redirect_uris} = credentials.installed;
-    const oAuth2Client = new google.auth.OAuth2(
-        client_id, client_secret, redirect_uris[0]);
-  
-    // Check if we have previously stored a token.
-    fs.readFile(TOKEN_PATH, (err, token) => {
-      if (err) return getAccessToken(oAuth2Client, callback);
-      oAuth2Client.setCredentials(JSON.parse(token));
-      callback(oAuth2Client);
-    });
-  };
+async function authorize(credentials) {
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  try {
+    const token = await readFileAsync(TOKEN_PATH);
+    oAuth2Client.setCredentials(JSON.parse(token));
+  } catch (err) {
+    return getAccessToken(oAuth2Client);
+  }
+  return oAuth2Client;
+};
 
 (async () => {
-  
-  try{
-    const credential =  await util.promisify(fs.readFile)('credentials.json');
-    const autorizzazione = await Promise.promisify(authorize(JSON.parse(credential)));
-    listFiles(autorizzazione);
+  try {
+    const credential = await readFileAsync('credentials.json');
+    const autorizzazione = await authorize(JSON.parse(credential));
+    await listFiles(autorizzazione);
   }
-  catch(error){
+  catch (error) {
     console.log(error);
   }
-
-
-
 })();
